5084b34e7ff90e57b77055cd7e40a8e652a85209
ilker balcılar <ilkerbalcilartr@gmail.com>
2025-06-21 23:45:35 +0300

    resize canvas

--- client/src/constants/canvas.ts
+++ client/src/constants/canvas.ts
@@ -1,6 +1,6 @@
 import { Tool } from "@/types/canvas";
 
-export const CANVAS_WIDTH = 3560;
+export const CANVAS_WIDTH = 2000;
 export const CANVAS_HEIGHT = 2000;
 export const DEFAULT_PIXEL_SIZE = 20;
 export const DEFAULT_COLOR = "#000000";
